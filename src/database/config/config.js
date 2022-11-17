bc3a240b9bd4aac2fa6e9b25d29c4abe9c8b10a3
Mario Rovira <xrovmario@yahoo.es>
2022-11-17 16:51:41 -0500

    Comenzado a configurar sequalizer

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,23 +1,23 @@
 module.exports{
   "development": {
-    "username": "root",
-    "password": null,
+    "username": "streamhouse6",
+    "password": "DigitalHouse6?",
     "database": "database_development",
-    "host": "127.0.0.1",
+    "host"    : "168.194.198.1",
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
+    "username": "streamhouse6",
+    "password": "DigitalHouse6?",
     "database": "database_test",
-    "host": "127.0.0.1",
+    "host"    : "168.194.198.1",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
+    "username": "streamhouse6",
+    "password": "DigitalHouse6?",
     "database": "database_production",
-    "host": "127.0.0.1",
+    "host"    : "168.194.198.1",
     "dialect": "mysql"
   }
 }
---
+++ src/database/models/user.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+    const Usuario = sequelize.define('usuario', {
+        id: {allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: DataTypes.INTEGER
+        },
+        nombres  : {
+            allowNull: false,
+            type:DataTypes.STRING
+        },
+        apellidos: {
+            allowNull: false,
+            type:DataTypes.STRING
+        },
+        email    : {
+            allowNull: false,
+            type:DataTypes.STRING
+        },
+        admin    : {
+            allowNull: true,
+            defaultValue: 0,
+            type: DataTypes.CHAR
+        },
+        local_id : {
+            type: DataTypes.INTEGER
+        },
+        estado   : {
+            allowNull: true,
+            defaultValue: 1,
+            type: DataTypes.CHAR
+        }
+    }, {});
+    Usuario.associate = function(models) {
+        // associations can be defined here
+    };
+    return Usuario;
+};
