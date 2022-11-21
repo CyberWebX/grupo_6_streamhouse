d5584d46dca1f4de656cc2a4cd444e03ea859bab
Mario Rovira <xrovmario@yahoo.es>
2022-11-20 23:26:22 -0500

    corrigiendo errores en el config de sequelize

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,23 +1,10 @@
-module.exports{
+module.exports = 
+{
   "development": {
-    "username": "streamhouse6",
-    "password": "DigitalHouse6?",
-    "database": "database_development",
-    "host"    : "168.194.198.1",
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": "streamhouse6",
-    "password": "DigitalHouse6?",
-    "database": "database_test",
-    "host"    : "168.194.198.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "streamhouse6",
-    "password": "DigitalHouse6?",
-    "database": "database_production",
-    "host"    : "168.194.198.1",
+    "username": "cyberweb_streamhouse6",
+    "password": "DigitaHouse6?",
+    "database": "cyberweb_streamhouse",
+    "host": "168.194.198.1",
     "dialect": "mysql"
   }
 }
